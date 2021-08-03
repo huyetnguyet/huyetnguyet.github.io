4e346c013bae3505be3d1c8662eb6f95bb6c4885
letuan317 <letuan317@gmail.com>
2021-08-03 13:06:12 -0700

    fix ads horizontal to none when mobile

--- src/components/adsMethods.js
+++ src/components/adsMethods.js
@@ -3,29 +3,14 @@ import AdSense from "react-adsense";
 const client = "ca-pub-9670179252828195";
 const slot_square = "6478931561";
 const slot_horizontal = "1992653961";
-const slot_vertical_ = "5391470746";
+const slot_vertical = "5391470746";
 
 export function AdsVertical() {
   return (
     <div className="adsVertical">
       <AdSense.Google
         client={client}
-        slot={slot_vertical_}
-        style={{ display: "block" }}
-        format="auto"
-        responsive="true"
-        layoutKey="-gw-1+2a-9x+5c"
-      />
-    </div>
-  );
-}
-
-export function AdsHorizontal() {
-  return (
-    <div className="adsHorizontal sticky">
-      <AdSense.Google
-        client={client}
-        slot={slot_horizontal}
+        slot={slot_vertical}
         style={{ display: "block" }}
         format="auto"
         responsive="true"
@@ -40,12 +25,28 @@ export function AdsVerticalHomeContent() {
     <div className="adsVerticalHome">
       <AdSense.Google
         client={client}
-        slot={slot_vertical_}
+        slot={slot_vertical}
         style={{ display: "block" }}
         format="auto"
         responsive="true"
         layoutKey="-gw-1+2a-9x+5c"
       />
+    </div>
+  );
+}
+export function AdsHorizontal() {
+  return (
+    <div className="adsHorizontal ">
+      <div className="sticky">
+        <AdSense.Google
+          client={client}
+          slot={slot_horizontal}
+          style={{ display: "block" }}
+          format="auto"
+          responsive="true"
+          layoutKey="-gw-1+2a-9x+5c"
+        />
+      </div>
     </div>
   );
 }
